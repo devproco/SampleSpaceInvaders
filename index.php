da4d2f48a9c234ebe502c02a7c67535dbb3ae4f5
vagrant <vagrant@local.puphpet>
2015-07-13 20:23:59 +0000

    Some minor style adjustments (still plain)

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 <html>
 <head>
     <title>Running Space Invaders...</title>
+    <style type="text/css">
+        body {
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            margin: 20px;
+            padding: 30px;
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
+
+<h1>Space Invaders Sample</h1>
+<div class="note">
+    The following output describes objects generated from a sample game: Space Invaders!
+</div>
 
 <pre><code>
 <?php
@@ -14,9 +27,8 @@ $UFO_factory = new UFOSelector;
 
 $grunt_ship = $UFO_factory->initShip("UFO");
 var_dump($grunt_ship);
-echo "\n";
 var_dump($grunt_ship->outputStatusText());
-echo "\n\n";
+
 
 $rocket_factory = new RocketSelector;
 
@@ -26,6 +38,7 @@ var_dump($standard_rocket->outputStatusText());
 
 ?>
 </code></pre>
+
 
 </body>
 </html>
