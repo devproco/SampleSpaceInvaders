eb3657217b042f1e476e546a070eecc6a0583b04
vagrant <vagrant@local.puphpet>
2015-07-13 20:25:39 +0000

    Respacing

--- index.php
+++ index.php
@@ -27,13 +27,15 @@ $UFO_factory = new UFOSelector;
 
 $grunt_ship = $UFO_factory->initShip("UFO");
 var_dump($grunt_ship);
+echo "\n";
 var_dump($grunt_ship->outputStatusText());
-
+echo "\n\n\n";
 
 $rocket_factory = new RocketSelector;
 
 $standard_rocket = $rocket_factory->initShip('Rocket');
 var_dump($standard_rocket);
+echo "\n";
 var_dump($standard_rocket->outputStatusText());
 
 ?>
